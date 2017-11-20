e465e83454402780157bc83d9424bc4ec81cd4fb
wanglei <wanglei1@zywzmail.com>
2017-11-20 17:02:40 +0800

    修改测试类名称

--- example-demo/cloud-register-demo/src/test/java/com/wl/CloudRegisterApplicationTests.java
+++ example-demo/cloud-register-demo/src/test/java/com/wl/CloudRegisterApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CloudBaseApplicationTests {
+public class CloudRegisterApplicationTests {
 
 	@Test
 	public void contextLoads() {
